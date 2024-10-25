0624da4615e042fcb7e4fb344d71a2fc18cd007b
febdanielh <febriandanielh09@gmail.com>
2024-10-25 10:51:51 +0700

    Initial Commit

---
+++ Learn Core Data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn Core Data
+//
+//  Created by Febrian Daniel on 25/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn Core Data/Learn_Core_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Core_DataApp.swift
+//  Learn Core Data
+//
+//  Created by Febrian Daniel on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Core_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
